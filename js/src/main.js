7375958b40ed07a9c5a9cdc9d66acc52e5331375
Nate Druss <nate@nsideas.com>
2018-12-11 17:23:38 -0500

    Card expansion animation

--- js/src/main.js
+++ js/src/main.js
@@ -14,6 +14,16 @@ function randomContent() {
 for (let card of cards) {
   card.querySelector('.card-body--inner').innerHTML = randomContent();
   card.addEventListener('click', () => {
-    card.classList.toggle('expanded');
+    card.classList.add('animating');
+    if (!card.classList.contains('expanded')) {
+      card.classList.add('expanded');
+      document.body.classList.add('no-scroll');
+    } else {
+      card.classList.remove('expanded');
+      document.body.classList.remove('no-scroll');
+    }
+    setTimeout(() => {
+      card.classList.remove('animating');
+    }, 1000)
   });
 }
