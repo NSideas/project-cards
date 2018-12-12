e3ee8546b2332df31cc19060cfd706e97f249690
Nate Druss <nate@nsideas.com>
2018-12-12 13:50:29 -0500

    Open and close card functions

--- js/src/main.js
+++ js/src/main.js
@@ -5,30 +5,45 @@ function randomContent() {
   let html  = `<h2>${chance.sentence()}</h2>`;
       html += `<h4>${chance.sentence({ words: 5 })}</h4>`;
       html += `<p>${chance.paragraph()}</p>`;
+      html += `<p>${chance.paragraph()}</p>`;
   return html;
+}
+
+function openCard(el, delay) {
+  el.classList.add('expanded', 'anim-in');
+  document.body.classList.add('no-scroll');
+  setTimeout(() => {
+    el.classList.remove('anim-in');
+  }, delay);
+}
+
+function closeCard(el, delay) {
+  el.classList.add('anim-out');
+  document.body.classList.remove('no-scroll');
+  setTimeout(() => {
+    el.classList.remove('expanded', 'anim-out');
+  }, delay);
+}
+
+function translateCard(el) {
+  el.style.transform = 'scaleX(.95) translateY(-389px)';
 }
 
 function cardExpansion(el) {
   let delay = 250;
   if (!el.classList.contains('expanded')) {
-    el.classList.add('expanded', 'anim-in');
-    document.body.classList.add('no-scroll');
-    setTimeout(() => {
-      el.classList.remove('anim-in');
-    }, delay);
+    // translateCard(el);
+    openCard(el, delay);
   } else {
-    el.classList.add('anim-out');
-    document.body.classList.remove('no-scroll');
-    setTimeout(() => {
-      el.classList.remove('expanded', 'anim-out');
-    }, delay);
+    closeCard(el, delay);
   }
 }
 
 for (let card of cards) {
   card.querySelector('.card-body--inner').innerHTML = randomContent();
   let cardHeader = card.querySelector('.card-header');
-  cardHeader.addEventListener('click', () => {
+  cardHeader.addEventListener('click', (e) => {
+    e.preventDefault();
     cardExpansion(card);
   });
 }
