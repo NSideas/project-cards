47560f306d59d47fa2cd95bfa1296e91ebc3c648
Nate Druss <nate@nsideas.com>
2018-12-13 10:06:42 -0500

    Created CSS variable for card height

--- js/src/main.js
+++ js/src/main.js
@@ -5,7 +5,6 @@ let cardHeight = 360;
 
 function randomContent() {
   let html  = `<h2>${chance.sentence()}</h2>`;
-      // html += `<h4>${chance.sentence({ words: 5 })}</h4>`;
       html += `<h4>${chance.company()}</h4>`;
       html += `<p>${chance.paragraph()}</p>`;
       html += `<p>${chance.paragraph()}</p>`;
@@ -17,7 +16,6 @@ function positionCard(index) {
   const y = index * cardHeight;
   const margin = cardWrapper.clientWidth * 0.025;
   el.style.transform = `scaleX(.95) translateY(${y + margin * index}px)`;
-
 }
 
 function openCard(index, delay) {
@@ -50,14 +48,9 @@ function closeCard(index, delay) {
   }, delay);
 }
 
-function translateCard(el) {
-  el.style.transform = 'scaleX(.95) translateY(-389px)';
-}
-
 function cardExpansion(index) {
   let delay = 250;
   if (!cards[index].classList.contains('expanded')) {
-
     openCard(index, delay);
   } else {
     closeCard(index, delay);
@@ -65,20 +58,13 @@ function cardExpansion(index) {
 }
 
 
-
 for (let i = 0; i < cards.length; i++) {
   positionCard(i);
   cards[i].querySelector('.card-body--inner').innerHTML = randomContent();
   let cardHeader = cards[i].querySelector('.card-header');
-  cardHeader.querySelector('h1').innerHTML = chance.animal();
+  cardHeader.querySelector('h1').innerHTML = `${chance.animal()} ${chance.animal()}`;
   cardHeader.addEventListener('click', (e) => {
     e.preventDefault();
-
-
-    // cards[cards.length - 1].style.marginBottom = '50vh';
-    // cards[i].scrollIntoView();
-
     cardExpansion(i);
-
   });
 }
